430c8638bda10f28b0643d4f8c92ceb0abc9f1d0
evastepanek <estepanek@student.tgm.ac.at>
2026-04-01 22:11:13 +0200

    fix e2e workflow

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     // preview (test:e2e): http://localhost:4173 | dev (test:e2e:dev): http://localhost:4173
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://localhost:5173',
   },
   component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
